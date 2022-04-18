366346d2574e3301e83e8a10fd1924b7938d0d78
Ordinatuer <ordinatuer@yandex.ru>
2022-04-18 22:05:05 +0300

    Подключен Vue.js

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,11 +1,13 @@
-import { startStimulusApp } from '@symfony/stimulus-bridge';
+import {createApp} from 'vue'
 
-// Registers Stimulus controllers from controllers.json and in the controllers/ directory
-export const app = startStimulusApp(require.context(
-    '@symfony/stimulus-bridge/lazy-controller-loader!./controllers',
-    true,
-    /\.[jt]sx?$/
-));
+const app = createApp({
+    //el: '#app',
+    delimiters:['${', '}$'],
+    data: () => ({
+        message: 'String from Vue.js'
+    })
+})
 
-// register any custom, 3rd party controllers here
-// app.register('some_controller_name', SomeImportedController);
+app.mount('#app')
+
+console.log(app);
